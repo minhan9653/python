48214db78913bf2ae72e3b196841877a3f933f10
minhan <minhan9653@naver.com>
2023-12-25 20:55:13 +0900

    2023-12-25오후

---
+++ 배열/과제 안낸사람 찾기.py
@@ -0,0 +1,14 @@
+submitted_numbers = set()
+
+# 출석번호를 28명만큼 입력받아 세트에 추가
+for _ in range(8):
+    student_number = int(input())
+    submitted_numbers.add(student_number)
+
+# 누락된 출석번호 찾기
+missing_numbers = list(set(range(1, 11)) - submitted_numbers)
+
+# 누락된 출석번호 오름차순 정렬 후 출력
+missing_numbers.sort()
+print(missing_numbers[0])  # 가장 작은 출석번호
+print(missing_numbers[1])  # 그 다음 출석번호
